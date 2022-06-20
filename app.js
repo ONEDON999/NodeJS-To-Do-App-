718e417a8b8975e4effbdfa95c2c66078ff65b3f
Nii-Gyan Christian <niigyanchristian@gmail.com>
2022-06-20 01:31:11 +0000

    Set Up the Server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.send("hello world");
+});
+
+app.listen(3000,()=>{
+    console.log("App is running on port 3000");
+});
