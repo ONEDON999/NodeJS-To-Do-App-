c8a746f7c0dd18216893e6d972a41e0fc88497ee
Nii-Gyan Christian <niigyanchristian@gmail.com>
2022-07-01 18:55:01 +0000

    route handling and templating

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/",(req,res)=>{
-    res.send("hello world");
+    res.render("index");
 });
+
+app.get("/about",(req,res)=>{
+    res.render("about");
+})
 
 app.listen(3000,()=>{
     console.log("App is running on port 3000");
